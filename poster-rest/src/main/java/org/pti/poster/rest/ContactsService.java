ef1b12fa48f0b16b3315e0797f4e7eef7a1dc132
vova <volodymyr.kozubal@goeuro.com>
2015-03-09 13:49:37 +0200

    fix the spring boot

--- poster-rest/src/main/java/org/pti/poster/rest/ContactsService.java
+++ poster-rest/src/main/java/org/pti/poster/rest/ContactsService.java
@@ -1,8 +1,15 @@
 package org.pti.poster.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContactsService {
+
+    @RequestMapping("/")
+    public  String test(){
+        return "Up";
+
+    }
 
 }
