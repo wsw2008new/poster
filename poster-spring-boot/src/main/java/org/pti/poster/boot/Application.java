5d4fda2383ca1132b112bf60f31e92e120c630e7
zeksa <moandorchik@gmail.com>
2015-03-10 23:31:59 +0200

    [PO-8] removed souts from Application

--- poster-spring-boot/src/main/java/org/pti/poster/boot/Application.java
+++ poster-spring-boot/src/main/java/org/pti/poster/boot/Application.java
@@ -8,8 +8,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 @ComponentScan
 @Import(value = SpringApplicationConfiguration.class)
@@ -17,14 +15,6 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
     }
 
 }
