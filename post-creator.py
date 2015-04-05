453efcf9e14a43e58ef021827061f7aaaa6c1021
zeksa <moandorchik@gmail.com>
2015-04-05 22:09:49 +0300

    [PO-8] added post creation script

---
+++ post-creator.py
@@ -0,0 +1,18 @@
+import os;
+import string;
+import random;
+import requests;
+import json
+
+def text_generator(size=240, chars=string.ascii_uppercase + string.digits):
+  return ''.join(random.choice(chars) for _ in range(size))
+
+def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+  return ''.join(random.choice(chars) for _ in range(size))
+
+for x in range(0,512):
+  url = "http://localhost:8080/api/post/save"
+  data = {"text": text_generator(),"userId": id_generator()}
+  headers = {'Content-type': 'application/json'}
+  r = requests.post(url, data=json.dumps(data), headers=headers)  
+  print(r.status_code, r.reason)
