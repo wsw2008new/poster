e5c91f67fafc6cb7f007453e5ee7631b4a08f61f
zeksa <moandorchik@gmail.com>
2015-04-08 02:17:55 +0300

    [PO-8] added springloaded

--- poster-spring-boot/src/main/java/org/pti/poster/boot/config/OAuthConfiguration.java
+++ poster-spring-boot/src/main/java/org/pti/poster/boot/config/OAuthConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -15,6 +16,7 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @Configuration
 @EnableResourceServer
