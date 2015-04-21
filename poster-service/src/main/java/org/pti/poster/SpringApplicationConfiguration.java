0c0e5a3311bdbc7dbb1d5f9009d9f590f7931761
zeksa <moandorchik@gmail.com>
2015-04-21 16:48:07 +0300

    [

--- poster-service/src/main/java/org/pti/poster/SpringApplicationConfiguration.java
+++ poster-service/src/main/java/org/pti/poster/SpringApplicationConfiguration.java
@@ -1,6 +1,11 @@
 package org.pti.poster;
 
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
 import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +16,24 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableSwagger
 @ComponentScan
 public class SpringApplicationConfiguration {
+
+	private SpringSwaggerConfig springSwaggerConfig;
+
+	@Autowired
+	public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+		this.springSwaggerConfig = springSwaggerConfig;
+	}
+
+	@Bean
+	public SwaggerSpringMvcPlugin customImplementation() {
+		return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(
+				apiInfo()).includePatterns("/api/.*");
+	}
+
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfo("Poster API", "API for Poster",
+				"Licensed under the Apache License, Version 2.0", "https://github.com/zeksa/poster",
+				"Apache License, Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+		return apiInfo;
+	}
 }
--- poster-service/src/main/java/org/pti/poster/config/WebConfiguration.java
+++ poster-service/src/main/java/org/pti/poster/config/WebConfiguration.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -26,5 +28,17 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 		httpMessageConverters.add(new RegisteredPostConverter(filterConfig, MediaType.APPLICATION_JSON));
 		httpMessageConverters.add(new UnregisteredPostConverter(filterConfig, MediaType.APPLICATION_JSON));
 		httpMessageConverters.add(new PostCollectionConverter(filterConfig, MediaType.APPLICATION_JSON));
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/swagger/**")
+				.addResourceLocations("classpath:/static/swagger/");
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new MappingJackson2HttpMessageConverter());
+		super.extendMessageConverters(converters);
 	}
 }
