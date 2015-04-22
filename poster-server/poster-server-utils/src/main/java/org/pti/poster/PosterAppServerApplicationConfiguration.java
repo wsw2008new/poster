ffed9afadb647fa5099c19d2b8fc4f1bcd2c6ee2
zeksa <moandorchik@gmail.com>
2015-04-22 18:36:30 +0300

    [PO-8] refactored smth

--- poster-server/poster-server-utils/src/main/java/org/pti/poster/PosterAppServerApplicationConfiguration.java
+++ poster-server/poster-server-utils/src/main/java/org/pti/poster/PosterAppServerApplicationConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
