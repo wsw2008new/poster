906eb3f3f81ccdae9d5fc1b8906d77a0f0c33fc8
zeksa <moandorchik@gmail.com>
2015-05-26 20:00:25 +0300

    [PO-9] added post select with userId

--- poster-gui/poster-client-browser/src/main/resources/static/poster/app/src/users/UserController.js
+++ poster-gui/poster-client-browser/src/main/resources/static/poster/app/src/users/UserController.js
@@ -1,97 +1,104 @@
-(function(){
+(function () {
 
-  angular
-       .module('users')
-       .controller('UserController', [
-          'userService', '$mdSidenav', '$mdBottomSheet', '$log', '$q',
-          UserController
-       ]);
-
-  /**
-   * Main Controller for the Angular Material Starter App
-   * @param $scope
-   * @param $mdSidenav
-   * @param avatarsService
-   * @constructor
-   */
-  function UserController( userService, $mdSidenav, $mdBottomSheet, $log, $q) {
-    var self = this;
-
-    self.selected     = null;
-    self.users        = [ ];
-    self.selectUser   = selectUser;
-    self.toggleList   = toggleUsersList;
-    self.share        = share;
-
-    // Load all registered users
-
-    userService
-          .loadAll()
-          .then( function( users ) {
-            self.users    = users;
-            self.selected = users[0];
-            console.log(users);
-          });
-
-    // *********************************
-    // Internal methods
-    // *********************************
+    angular
+        .module('users')
+        .controller('UserController', [
+            'userService', '$mdSidenav', '$mdBottomSheet', '$log', '$q',
+            UserController
+        ]);
 
     /**
-     * First hide the bottomsheet IF visible, then
-     * hide or Show the 'left' sideNav area
+     * Main Controller for the Angular Material Starter App
+     * @param $scope
+     * @param $mdSidenav
+     * @param avatarsService
+     * @constructor
      */
-    function toggleUsersList() {
-      var pending = $mdBottomSheet.hide() || $q.when(true);
+    function UserController(userService, $mdSidenav, $mdBottomSheet, $log, $q) {
+        var self = this;
 
-      pending.then(function(){
-        $mdSidenav('left').toggle();
-      });
-    }
+        self.selected = null;
+        self.users = [];
+        self.selectUser = selectUser;
+        self.toggleList = toggleUsersList;
+        self.share = share;
 
-    /**
-     * Select the current avatars
-     * @param menuId
-     */
-    function selectUser ( user ) {
-      self.selected = angular.isNumber(user) ? $scope.users[user] : user;
-      self.toggleList();
-    }
+        // Load all registered users
 
-    /**
-     * Show the bottom sheet
-     */
-    function share($event) {
-        var user = self.selected;
+        userService
+            .loadAll()
+            .then(function (users) {
+                self.users = users;
+                self.selected = users[0];
+                console.log(users);
+            });
 
-        $mdBottomSheet.show({
-          parent: angular.element(document.getElementById('content')),
-          templateUrl: '/src/users/view/contactSheet.html',
-          controller: [ '$mdBottomSheet', UserSheetController],
-          controllerAs: "vm",
-          bindToController : true,
-          targetEvent: $event
-        }).then(function(clickedItem) {
-          clickedItem && $log.debug( clickedItem.name + ' clicked!');
-        });
+        // *********************************
+        // Internal methods
+        // *********************************
 
         /**
-         * Bottom Sheet controller for the Avatar Actions
+         * First hide the bottomsheet IF visible, then
+         * hide or Show the 'left' sideNav area
          */
-        function UserSheetController( $mdBottomSheet ) {
-          this.user = user;
-          this.items = [
-            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-          ];
-          this.performAction = function(action) {
-            $mdBottomSheet.hide(action);
-          };
-        }
-    }
+        function toggleUsersList() {
+            var pending = $mdBottomSheet.hide() || $q.when(true);
 
-  }
+            pending.then(function () {
+                $mdSidenav('left').toggle();
+            });
+        }
+
+        /**
+         * Select the current avatars
+         * @param menuId
+         */
+        function selectUser(user) {
+            self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+            self.toggleList();
+
+            userService
+                .loadAllPostsForUser(user.userId)
+                .then(function (posts) {
+                    self.currentPosts = posts;
+                    console.log(posts);
+                });
+        }
+
+        /**
+         * Show the bottom sheet
+         */
+        function share($event) {
+            var user = self.selected;
+
+            $mdBottomSheet.show({
+                parent: angular.element(document.getElementById('content')),
+                templateUrl: '/src/users/view/contactSheet.html',
+                controller: ['$mdBottomSheet', UserSheetController],
+                controllerAs: "vm",
+                bindToController: true,
+                targetEvent: $event
+            }).then(function (clickedItem) {
+                clickedItem && $log.debug(clickedItem.name + ' clicked!');
+            });
+
+            /**
+             * Bottom Sheet controller for the Avatar Actions
+             */
+            function UserSheetController($mdBottomSheet) {
+                this.user = user;
+                this.items = [
+                    {name: 'Phone', icon: 'phone', icon_url: 'assets/svg/phone.svg'},
+                    {name: 'Twitter', icon: 'twitter', icon_url: 'assets/svg/twitter.svg'},
+                    {name: 'Google+', icon: 'google_plus', icon_url: 'assets/svg/google_plus.svg'},
+                    {name: 'Hangout', icon: 'hangouts', icon_url: 'assets/svg/hangouts.svg'}
+                ];
+                this.performAction = function (action) {
+                    $mdBottomSheet.hide(action);
+                };
+            }
+        }
+
+    }
 
 })();
--- poster-gui/poster-client-browser/src/main/resources/static/poster/app/src/users/UserService.js
+++ poster-gui/poster-client-browser/src/main/resources/static/poster/app/src/users/UserService.js
@@ -16,25 +16,19 @@
      * @constructor
      */
     function UserService($q, $resource) {
-        var users = [{
-            "userId": "2e8ba4c0-d149-4ab7-be64-983545c4432c",
-            "userNickName": "qdqd",
-            "userName": "dqqq",
-            "errorMessages": []
-        }, {
-            "userId": "fad34e32-105e-4d5a-8ab6-5f1502140a17",
-            "userNickName": "qd12123qd",
-            "userName": "dqq313113q",
-            "errorMessages": []
-        }];
-
-        // Promise-based API
         return {
             loadAll: function () {
                 // Simulate async nature of real remote calls
                 var AllUsers = $resource('/api/user/registered/all/', {});
                 var allUsers = AllUsers.query();
                 return $q.when(allUsers);
+            },
+
+            loadAllPostsForUser: function (id) {
+                // Simulate async nature of real remote calls
+                var AllPosts = $resource('/api/post/get/user/'+id, {}, {'query': {method: 'GET', isArray: false}});
+                var allPosts = AllPosts.query();
+                return $q.when(allPosts);
             }
         };
     }
