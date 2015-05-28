44c994aad194a1a7a65337900d749cf16c4ab8b0
zeksa <moandorchik@gmail.com>
2015-05-28 16:19:14 +0300

    added hazelcast config

--- poster-service/src/main/java/org/pti/poster/hazelcast/HazelcastConfiguration.java
+++ poster-service/src/main/java/org/pti/poster/hazelcast/HazelcastConfiguration.java
@@ -1,11 +1,13 @@
 package org.pti.poster.hazelcast;
 
-import com.hazelcast.config.Config;
+import com.hazelcast.config.*;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class HazelcastConfiguration {
@@ -15,6 +17,26 @@ public class HazelcastConfiguration {
 	@PostConstruct
 	public void init() {
 		Config hazelcastConfig = new Config();
+		NetworkConfig networkConfig=new NetworkConfig();
+		InterfacesConfig interfacesConfig=new InterfacesConfig();
+		JoinConfig joinConfig=new JoinConfig();
+		MulticastConfig multicastConfig=new MulticastConfig();
+		TcpIpConfig tcpIpConfig=new TcpIpConfig();
+		tcpIpConfig.setEnabled(true);
+		multicastConfig.setEnabled(false);
+
+		List<String> members=new ArrayList<>();
+		members.add("127.0.0.1");
+
+		tcpIpConfig.setMembers(members);
+
+		joinConfig.setMulticastConfig(multicastConfig);
+		joinConfig.setTcpIpConfig(tcpIpConfig);
+		interfacesConfig.addInterface("127.0.0.1");
+		interfacesConfig.setEnabled(true);
+		networkConfig.setInterfaces(interfacesConfig);
+		networkConfig.setJoin(joinConfig);
+		hazelcastConfig.setNetworkConfig(networkConfig);
 		hazelcastInstance = Hazelcast.newHazelcastInstance(hazelcastConfig);
 	}
 
