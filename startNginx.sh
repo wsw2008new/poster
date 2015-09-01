fb4e2cc7fdfadd3924df76fc62987f8bef38fc3d
vyacheslav.mysak <vyacheslav.mysak@engagepoint.com>
2015-09-01 13:42:40 +0300

    some changes with ip bindings

--- startNginx.sh
+++ startNginx.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-mkdir /var/log/nginx
+pkill nginx
+rm -rf /var/log/nginx && mkdir /var/log/nginx
+rm -f /etc/nginx/conf.d/poster.conf
+cp ./nginx-config /etc/nginx/conf.d/poster.conf
 
 systemctl start nginx.service
 systemctl status nginx.service
